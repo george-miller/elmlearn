d834f80ed2cd0c6c0273bfdd5bab744f0d74abf1
George Miller <geo.m.miller@gmail.com>
2017-09-09 12:04:26 -0600

    Start create post

--- Main.elm
+++ Main.elm
@@ -2,79 +2,89 @@ module Main exposing (..)
 
 import Html.Attributes exposing (style)
 import Html exposing (Html, beginnerProgram, div, text, button, p, input, form)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput, on)
+
 
 main =
     beginnerProgram { model = model, view = view, update = update }
 
-type alias Post = {
-        message : String,
-        votes:Int,
-        id:Int
+
+type alias Post =
+    { message : String
+    , votes : Int
+    , id : Int
     }
 
-type alias Model = List Post
+
+type alias Model =
+    List Post
 
 
 type Msg
     = VoteUp Int
     | VoteDown Int
-    | CreatePost
+    | CreatePost String
 
 
 model : Model
 model =
-    [
-     { message = "poo"
+    [ { message = "poo"
       , votes = 0
       , id = 0
-     },
-     { message = "poo2"
+      }
+    , { message = "poo2"
       , votes = 0
       , id = 1
-     }
+      }
     ]
 
 
 updatePost : Int -> (Post -> Post) -> Model -> Model
 updatePost id f model =
-    List.map (\post ->
-             if post.id == id then
-                 f post
-             else post) model
+    List.map
+        (\post ->
+            if post.id == id then
+                f post
+            else
+                post
+        )
+        model
+
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         VoteUp id ->
-            updatePost id (\post -> {post | votes = post.votes + 1}) model
+            updatePost id (\post -> { post | votes = post.votes + 1 }) model
 
         VoteDown id ->
-            updatePost id (\post -> {post | votes = post.votes - 1}) model
+            updatePost id (\post -> { post | votes = post.votes - 1 }) model
 
-        CreatePost ->
-            model
+        CreatePost msg ->
+            { id = 3, message = msg, votes = 0 } :: model
 
 
 view : Model -> Html Msg
 view model =
-    div [] <| (List.map viewPost model) ++ [createPostForm]
+    div [] <| (List.map viewPost model) ++ [ createPostForm ]
 
 
 createPostForm : Html Msg
-createPostForm = form [style [("clear", "both")]] [
-                   input [] []
-                 ]
+createPostForm =
+    form [ style [ ( "clear", "both" ) ] ]
+        [ input [ on "keydown" (Json.map ta) ] []
+        ]
+
 
 viewPost : Post -> Html Msg
 viewPost post =
-    div [style [("clear", "both")]] [
-         div [style [("float", "left")]] [
-               p [] [text <| "Topic: " ++ post.message],
-               p [] [text <| "Votes: " ++ (toString post.votes)]
-             ],
-         div [style [("float", "left")]] [
-               div [] [button [onClick <| VoteUp post.id] [text "Up"]],
-               div [] [button [onClick <| VoteDown post.id] [text "Down"]]
-             ]
+    div [ style [ ( "clear", "both" ) ] ]
+        [ div [ style [ ( "float", "left" ) ] ]
+            [ p [] [ text <| "Topic: " ++ post.message ]
+            , p [] [ text <| "Votes: " ++ (toString post.votes) ]
+            ]
+        , div [ style [ ( "float", "left" ) ] ]
+            [ div [] [ button [ onClick <| VoteUp post.id ] [ text "Up" ] ]
+            , div [] [ button [ onClick <| VoteDown post.id ] [ text "Down" ] ]
+            ]
         ]
